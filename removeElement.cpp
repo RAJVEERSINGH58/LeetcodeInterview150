979d761f39d2071e78aae38c0387d559fd728725
Rajveer Singh <pheonixrajveer@gmail.com>
2025-07-30 00:02:47 +0530

    done

---
+++ removeElement.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+
+        if(nums.size()==0) return 0;
+        int i = 0;
+        for(int j = 0 ; j < nums.size() ; j++){
+            if(nums[j]!=val){
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+};
