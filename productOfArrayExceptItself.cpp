156176e887be6d4e01d9ea7a9fb6c9680c6848c3
Rajveer Singh <pheonixrajveer@gmail.com>
2025-08-09 21:51:11 +0530

    h

---
+++ productOfArrayExceptItself.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> pre(n);
+        int p = nums[0];
+        pre[0] = 1;
+        for(int i = 1 ; i < n ; i++){
+            pre[i] = p;
+            p *= nums[i];
+        }
+        p = nums[n-1];
+        for(int i = n-2 ; i >= 0 ; i--){
+            pre[i] *= p;
+            p *= nums[i];
+        }
+        return pre;
+    }
+};
+
+int main() {
+    
+    return 0;
+}
