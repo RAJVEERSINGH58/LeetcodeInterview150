d6b9a25cdfa95f54270f04965a3202482ddabc76
Rajveer Singh <pheonixrajveer@gmail.com>
2025-08-26 14:42:14 +0530

    h

---
+++ sliding window/minimumSizeSubarraySum.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        
+        int n = nums.size();
+
+        int i = 0;
+        int j = 0;
+
+        int ans = INT_MAX;
+        int sum = 0;
+
+        while(j < n){
+            sum += nums[j];
+
+            while(sum >= target){
+                ans = min(ans , (j - i + 1));
+                sum -= nums[i];
+                i++;
+            }
+
+            j++;
+
+        }
+
+        return (ans == INT_MAX) ? 0 : ans;
+    }
+};
+
+int main() {
+    
+    return 0;
+}
